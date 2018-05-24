2edddf2053430d1c8acb5536e1f9f2f53bd38fac
Matthew <matthewfallon33@gmail.com>
2018-05-24 14:53:12 +0100

    Think I've got the slider partly working

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,2 +1,7 @@
-const carousel = document.getElementsByClassName("carousel");
-setInterval(function(){console.log("noooott")}, 1000);
+const carousel = document.getElementsByClassName("carousel")[0];
+var kids  = carousel.children.length;
+// setInterval(function(){console.log("noooott")}, 1000);
+carousel.addEventListener("mouseover", (e) => {
+  carousel.style.marginLeft += 100 + "%";
+  console.log(carousel.style.marginLeft);
+});
