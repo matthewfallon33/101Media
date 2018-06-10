255d7195b147fcbe752a661ee25d86cfd85c58dd
Matthew <matthewfallon33@gmail.com>
2018-06-10 21:17:28 +0100

    Got data onto view

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 var express = require("express");
 var path = require("path");
+var graph = require("fbgraph");
 var app = express();
+require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
-  res.render("index");
-})
+  let posts = [];
+  graph.setAccessToken(process.env.APP_TOKEN);
+  graph.get("/me/feed", {limit: 5, access_token: process.env.APP_TOKEN}, function(err, response) {
+    if(response.data){
+      for (var i = 0; i < response.data.length; i++) {
+        if(response.data[i].message !== undefined || response.data[i].message !== undefined){
+            posts.push(response.data[i]);
+        }
 
-app.listen(3000, () => {
+      }
+      console.log(response.data[1].message);
+      res.render("index", {posts: posts});
+    }else{
+      res.render("index", {err:"No Posts here :("});
+    }
+  });
+});
+
+app.listen(process.env.PORT, () => {
   console.log("App running on port 3000");
 });
