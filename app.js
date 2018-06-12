97b02497c0111bb48a438103b51afac5f3e35b61
Matthew <matthewfallon33@gmail.com>
2018-06-12 14:29:45 +0100

    Need to get a longer access token

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
+  // try get extended token use temp for meantime
   let posts = [];
   graph.setAccessToken(process.env.APP_TOKEN);
-  graph.get("/me/feed", {limit: 5, access_token: process.env.APP_TOKEN}, function(err, response) {
+  graph.get("/me/posts", {limit: 5, access_token: process.env.APP_TOKEN}, function(err, response) {
     if(response.data){
       for (var i = 0; i < response.data.length; i++) {
-        if(response.data[i].message !== undefined || response.data[i].message !== undefined){
+            if(response.data[i].message !== undefined || response.data[i].message !== undefined){
             posts.push(response.data[i]);
         }
-
       }
-      console.log(response.data[1].message);
       res.render("index", {posts: posts});
     }else{
       res.render("index", {err:"No Posts here :("});
